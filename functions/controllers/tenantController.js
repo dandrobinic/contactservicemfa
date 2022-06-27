344bbed8dd032c87607d7f09319b235a6de423c9
Daniel Alfredo Garcia Drobinic <dgard@nae.global>
2022-06-27 01:37:07 -0500

    Organizaing Functions test

--- functions/controllers/tenantController.js
+++ functions/controllers/tenantController.js
@@ -3,7 +3,11 @@ const db = require('../config/firebase');
 const getTenantInfo = async (req, res) => {
     try {
         const tenantInfo = await (await db.collection('tenant').doc('e6cq7lXUTzMqPZiyclqI').get()).data()
-        return res.status(200).json(tenantInfo) 
+        return res.status(200).json({
+            message: 'Claro Colombia Entrust Tenant Info',
+            data: tenantInfo
+        })
+        //return res.status(200).json(tenantInfo)
     } catch (error) {
         return res.status(500).json(error.message)
     }
